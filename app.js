3dad82f617e321306bf502f4eca4fcce41264a56
alscotty <alawrencescott@gmail.com>
2020-01-18 18:44:13 -0800

    validations

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
+const bodyParser = require('body-parser');
 const express = require("express");
-const app = express();
 const db = require('./config/keys').mongoURI;
+
+
+
+const app = express();
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Goodbye Cruel World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
