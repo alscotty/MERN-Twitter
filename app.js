055e229282c8bf2895599082f77f071527bcaa55
alscotty <alawrencescott@gmail.com>
2020-01-19 11:31:19 -0800

    user auth

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-const bodyParser = require('body-parser');
-const express = require("express");
-const db = require('./config/keys').mongoURI;
-
-
-
-const app = express();
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Goodbye Cruel World"));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+app.get("/", (req, res) => res.send("Hello World!!"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
